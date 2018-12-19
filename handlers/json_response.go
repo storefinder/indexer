9af4a9d9188e80c5bd298dd701189017d2fd092e
Ram Gopinathan <rprakashg@gmail.com>
2018-12-19 09:36:02 -0800

    Updated log package

--- handlers/json_response.go
+++ handlers/json_response.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"encoding/json"
-	"log"
+
+	log "github.com/sirupsen/logrus"
+
 	"net/http"
 )
 
@@ -27,9 +29,9 @@ func (jr *JSONResponse) Write(w http.ResponseWriter) {
 	//Write to response
 	w.Header().Set(contentTypeHeader, contentType)
 	w.WriteHeader(jr.status)
-	log.Printf("HTTP Status : %v", jr.status)
+	log.Infof("HTTP Status : %v", jr.status)
 	if err := json.NewEncoder(w).Encode(jr.data); err != nil {
-		log.Printf("Error Writing JSON to response")
+		log.Info("Error Writing JSON to response")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
